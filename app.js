9108a0354e092574ba7f0985cba5604cefdd30ec
Bishnudas Ghoshal <62611068+bishnudasghoshal@users.noreply.github.com>
2020-10-06 21:02:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us10.api.mailchimp.com/3.0/lists/857f808385";
+
+  const options = {
+    method: "POST",
+    auth: "bishnu123:1b10964c781e84aa355ec1cdc0350fef-us10"
+  }
+
+  const request = https.request(url, options, function(response){
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+
+  })
+
+  request.write(jsonData);
+  request.end();
+
+})
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000.");
+})
+
+// API Key
+// 1b10964c781e84aa355ec1cdc0350fef-us10
+
+// List ID
+// 857f808385
